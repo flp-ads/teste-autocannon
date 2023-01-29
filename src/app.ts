da0be7a8bb12e13987ae43643c5828349a75eaae
Felipe Silva <felipesilva.98ads@gmail.com>
2023-01-29 17:31:29 -0300

    feat: set timeout

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,17 @@
 import express, { Request, Response } from "express";
-import { prisma } from "./database/prismaClient";
-
-const createUser = async (req: Request, res: Response) => {
-  const { name, email } = req.body;
-  const user = await prisma.user.create({
-    data: {
-      name,
-      email,
-    },
-  });
-  res.status(201).json(user);
-};
 
 const getUsers = async (_: Request, res: Response) => {
-  const users = await prisma.user.findMany();
-  res.json(users);
+  const user = "felipe";
+  setTimeout(() => user, 300);
+  res.setTimeout(300, () => {
+    res.json({ message: "demorou 300ms" });
+  });
 };
 
 const app = express();
 
 app.use(express.json());
-app.post("/users", createUser);
+
 app.get("/users", getUsers);
 
 export { app };
